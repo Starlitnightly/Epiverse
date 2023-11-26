c17231394bd73536b7a5ac24ce8a83d9a0a5f888
DBinary <1157269262@qq.com>
2023-11-26 16:50:15 +0800

    update the function of download genome sequence

--- Epiverse/utils/_data.py
+++ Epiverse/utils/_data.py
@@ -17,6 +17,43 @@ def download_gene_annotation_file(download_file:str = ''):
                                      path=os.path.join(download_file,'{}.gtf.gz'.format(datasets_name)),title=datasets_name)
     print('......Gene Annotation File download finished!')
 
+def download_genome_sequence(genome_name='', download_folder=''):
+    """
+    Download genome sequence file for a specified genome and save it in a folder named after the genome.
+
+    Parameters:
+    - genome_name (str): The name of the genome for which to download the sequence file.
+    - download_folder (str): The local folder where the downloaded file will be saved.
+
+    Returns:
+    None
+
+    Note:
+    This function uses the data_downloader function to download genome sequence files from UCSC Genome Browser.
+    The downloaded file is saved in a folder named after the genome in the specified download folder.
+    """
+    # Mapping of genome names to their respective download URLs
+    _datasets = {
+        'hs1': 'https://hgdownload.soe.ucsc.edu/goldenPath/hs1/bigZips/hs1.fa.gz',
+        'hg38': 'https://hgdownload.soe.ucsc.edu/goldenPath/hg38/bigZips/hg38.fa.gz',
+        'hg19': 'https://hgdownload.soe.ucsc.edu/goldenPath/hg19/bigZips/hg19.fa.gz',
+        'mm10': 'https://hgdownload.soe.ucsc.edu/goldenPath/mm10/bigZips/mm10.fa.gz',
+        'mm39': 'https://hgdownload.soe.ucsc.edu/goldenPath/mm39/bigZips/mm39.fa.gz',
+    }
+
+    if genome_name in _datasets:
+        # Create a folder for the genome if it doesn't exist
+        download_path = os.path.join(download_folder, genome_name)
+        os.makedirs(download_path, exist_ok=True)
+
+        print('......Genome Sequence File download start:', genome_name)
+        # Download the genome sequence file and save it in the designated folder
+        file_path = data_downloader(url=_datasets[genome_name],
+                                     path=os.path.join(download_path, f'{genome_name}.fa.gz'), title=genome_name)
+        print('......Genome Sequence File download finished! Saved to:', file_path)
+    else:
+        print(f"Genome Sequence File '{genome_name}' not found in the dataset.")
+
 def download_gene_activity_reference(download_file:str = ''):
     r"""
     load reference data for calculating gene activity.
@@ -27,10 +64,10 @@ def download_gene_activity_reference(download_file:str = ''):
     }
      
     for datasets_name in _datasets.keys():
-        print('......Gene Annotation File download start:',datasets_name)
+        print('......Gene Activity Reference File download start:',datasets_name)
         model_path = data_downloader(url=_datasets[datasets_name],
                                      path=os.path.join(download_file,'{}.txt'.format(datasets_name)),title=datasets_name)
-    print('......Gene Annotation File download finished!')
+    print('......Gene Activity Reference File download finished!')
 
 def data_downloader(url,path,title):
     r"""datasets downloader
